02d76273ba199031053f7c1096a393dbb8689330
Alberto González <algorinetportfolio@gmail.com>
2019-05-08 18:42:44 +0200

    style-test

--- butler-front/components/SplashPage.js
+++ butler-front/components/SplashPage.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react'
-import { Text } from 'react-native';
+import React, { Component } from "react";
+import { Text, View ,Image} from "react-native";
+import styled from "styled-components/native";
 
+const Title = styled.Text`
+  color: palevioletred;
+  font-weight: bold;
+  font-size: 80px;
+`;
+
+const StyledView = styled.View`
+  padding: 5%;
+`;
 
 export default class SplashPage extends Component {
   render() {
     return (
-        <Text>SplashPage</Text>
-    )
+      <StyledView>
+        <Title>BUTLER</Title>
+        <Image
+          source={require('../assets/images/butlerIcon.png')}
+        />
+        
+      </StyledView>
+   
+    );
   }
 }
+
+
